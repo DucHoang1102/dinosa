5e8bd393f1eaad7adb832f0db48157d4dee698a8
Duc Hoang <ndhdldt1@gmail.com>
2017-07-15 01:37:47 +0000

    Create database: embryo_tshirt

---
+++ apps/database/migrations/2017_07_15_013548_embryo_tshirt.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class EmbryoTshirt extends Migration
+{
+    /**
+     * Bảng embryo_tshirt: id, name, description, price
+     * Description: Bảng danh sách phôi áo
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('embryo_tshirt', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('description');
+            $table->>bigInteger('price');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('embryo_tshirt');
+    }
+}
