f18029338f263b0b41cbcddb1e3f362a18c96455
DucHoang <ndhdldt1@gmail.com>
2018-04-28 09:45:49 +0000

    Fix products blade do copy nham

--- apps/resources/views/orders/prints/products.blade.php
+++ apps/resources/views/orders/prints/products.blade.php
@@ -2,136 +2,136 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-	<title>Print Orders</title>
-	<!-- Bootstrap 3.3.7 CSS -->
-	<link href="\dist\plugins\bootstrap\css\bootstrap.min.css" rel="stylesheet" type="text/css">
-
-	<!-- Jquery 3.2.1 -->
-	<script src="\dist\plugins\jquery\jquery-3.2.1.min.js" type="text/javascript"></script>
-
+	<title>Print products</title>
 	<style type="text/css">
-		.print {
-			padding: 5px 15px;
-			width: 100%;
-			height: 100%;
-		}
-		.print .labels {
-			position: relative;
-			padding: 7px 5px;
-			border: 2px solid black;
-			border-radius: 5px;
-			width: 100%;
-			height: 100%;
-		}
-		.print .labels h3{
-			color: #000000;
-			margin-top: 15px;
-			padding-top: 5px;
-			border-top: 2px solid #cecece;
-		}
-		.print .labels .glyphicon {
-			font-size: 0.8em;
-			margin-right: 0.3em;
-		}
-		.print .labels div {
-			position: relative;
-			margin-bottom: 5px;
-			border-bottom: 1px solid #eaeaea;
-		}
-		.print .labels-left {
-			display: inline-block;
-			font-size: 1.3em;
-			width: 20%;
-			text-align: right;
-			font-weight: bold;
-		}
-		.print .labels-right {
-			display: inline-block;
-			vertical-align: top;
-			padding-left: 3px;
-			font-size: 1.2em;
-			width: 79%;
-			color: #333333;
-			font-weight: bold;
-		}
-		.print .col-xs-6 {
-			overflow: hidden;
-			margin-top: 10px;
-			margin-bottom: 15px;
-			height: 370px;
-		}
-		.print .glyphicon-remove-circle {
-			cursor: pointer;
-			display: none;
-			position: absolute;
-			margin: 0px !important;
-			top: 5px;
-			right: 5px;
-			font-size: 1.9em !important;
-		}
-		.print .labels:hover .glyphicon-remove-circle{
-			display: block;
-		}
-		.print .labels:hover {
-			border: 2px solid red;
-		}
-		@media only print  {
-			.print .labels:hover {
+		@media only screen {
+			body {
+				cursor: pointer;
+				font-family: arial;
+				background: #F5F5F5;
+			}
+			.container-print-product {
+				max-width: 1300px;
+				margin: 0 auto;
+			}
+			.print-product {
+				padding-left: 50px;
+			}
+			.print-product .product {
+				position: relative;
+				overflow: hidden;
+				display: inline-block;
+				width: 200px;
+				padding: 5px;
+				margin: 10px;
 				border: 2px solid black;
 			}
-			.print .labels:hover .glyphicon-remove-circle {
+			.print-product .product:hover {
+				border: 2px solid red;
+			}
+			.print-product img {
+				max-width: 100%;
+			}
+			.print-product .del {
+				position: absolute;
+				background: #595959;
+				text-align: center;
+				color: white;
+				padding: 4px 8px;
+				top: 0px;
+				right: 0px;
+				font-size: 0.8em;
+			}
+			.print-product .del:hover {
+				background: black;
+			}
+			.print-product  .description {
+				position: absolute;
+				display: none;
+				padding-top: 15%;
+				padding-left: 3%;
+				width: 100%;
+				height: 100%;
+				top: 0px;
+				left: 0px;
+				background: rgba(255, 255, 255, 0.8);
+			}
+			.print-product .product:hover .description{
+				display: block;
+			}
+			.print-product  .description .info {
+				display: inline-block;
+				padding-left: 8%;
+				padding-bottom: 3%;
+			}
+			.print-product  .description b {
+				padding-bottom: 2%;
+				display: block;
+			}
+			.print-product  .description hr {
+				width: 90%;
+			}
+		}
+		@media only print  {
+			.print-product .product img {
+				display: block;
+				width: 100% !important;
+			}
+			.print-product .product .del, .print-product .product .description, hr, br {
 				display: none;
 			}
 		}
 	</style>
 </head>
 <body>
-	<div class="print">
-		@include('orders.prints.countEmbryoTshirt')
-
-		<div class="row">
-			@foreach ($orders_daXacNhan as $key => $order)
-				<div class="col-xs-6">
-					<div class="labels">
-						<h3 style="border:none; margin-top:0px;"><span class="glyphicon glyphicon-user" aria-hidden="true"></span>Thông khách hàng</h3>
-						<div>
-							<span class="labels-left">HỌ TÊN: </span>    <span class="labels-right">{{ $order->name or "" }}</span>
-						</div>
-						<div>
-							<span class="labels-left">SĐT: </span>       <span class="labels-right">{{ $order->phone or "" }}</span>
-						</div>
-						<div>
-							<span class="labels-left">ĐỊA CHỈ:</span>    <span class="labels-right">{{ $order->address or "" }}</span>
-						</div>
-						<div style="border: none">
-							<span class="labels-left">TỔNG THU: </span>  <span class="labels-right">{{ $order->total_money or 0 }} vnđ</span>
-						</div>
-						<h3><span class="glyphicon glyphicon glyphicon-home" aria-hidden="true"></span>Thông tin shop</h3>
-						<div>
-							<span class="">SDT : </span>  <span class=""><b>0972 982 082</b></span>
-						</div>
-						<div>
-							<span class="">FB : </span>   <span class=""><b>fb/aodoitinhyeu</b></span>
-						</div>
-						<div style="border:none">
-							<span class="">Chú ý :</span> <span class=""><b>Bưu tá vui lòng cho khách xem hàng và liên hệ với Shop trong trường hợp không giao được hàng. Sự phát triển của chúng tôi phụ thuộc phần lớn vào cách làm việc của các bạn. Cảm ơn rất nhiều!!!</b></span>
-						</div>
-						<span class="glyphicon glyphicon-remove-circle" aria-hidden="true"></span>
-					</div><!--/.labels-->
-				</div><!--/.col-xs-6-->
+	<div class="container-print-product">
+		<div class="print-product">
+			@foreach ($orders_daXacNhan as $order)
+				@foreach ($order->products as $product)
+					@if ($product->status == 1)
+						@continue;
+					@endif
+					<div class="product">
+						<img src="{{ $product->url_image }}">
+						<span class="description">
+							<b>Tên KH:</b> 
+							<span class="info"> {{ $order->name }}</span>
+							<hr>
+							<b>SDT:</b> 
+							<span class="info"> {{ $order->phone }}</span>
+							<hr>
+							<b>Sản phẩm | Mã:</b> 
+							<span class="info"> {{ $product->name }} | {{ $product->id_image_print }}</span>
+							<hr>
+							<b>Tổng tiền:</b> 
+							<span class="info">{{ $order->total_money }} vnđ</span>
+						</span>
+						<span class="del">XÓA</span>
+					</div>
+				@endforeach
+				<br>
 			@endforeach
-		</div><!--/.row-->
-	</div> <!--/.print-->
+		</div>
+	</div>
+
+	<!-- Jquery 3.2.1 -->
+	<script src="\dist\plugins\jquery\jquery-3.2.1.min.js" type="text/javascript"></script>
 
 	<script type="text/javascript">
-		(function ($) {
-			
-			$('.print').on('click', '.glyphicon-remove-circle', function(){
-				$(this).parent().parent().hide(400, function(){
+		(function($){
+			$('.print-product').on('click', '.product .del', function() {
+				$(this).parent().hide(500, function(){
 					$(this).remove();
 				});
 			});
-
+			// Fix lỗi 1 sản phẩm bị thừa 1 trang in
+			if ( $('.print-product .product').length == 1 ) {
+				$('.print-product .product').css({
+					position : 'absolute',
+					top      : '0px',
+					left     : '0px'
+				});
+			}
 		})(jQuery);
 	</script>
 </body>
