87ba25f1475c9ad219722d04c7c2d764c1c1ee45
DucHoang <ndhdldt1@gmail.com>
2018-04-29 22:35:06 +0000

    Fix git

--- apps/storage/framework/views/0c2f4aa2ce4fbac33312f608dd6c2fab67e9ebff.php
+++
@@ -1,7 +0,0 @@
-<div class="funs-thungrac">
-	<a class="delete-all" href="orders/delete-permanently/all">
-		<button type="button" class="btn btn-danger" data-toggle="tooltip" data-placement="top" title="Dọn dẹp thùng rác">
-			<span class="glyphicon glyphicon-trash" aria-hidden="true"></span> Dọn dẹp
-		</button>
-	</a>
-</div>
--- apps/storage/framework/views/6499bf2a8c8f9217fb6c7cc423a4af95d9ac0e66.php
+++
@@ -1,6 +0,0 @@
-<div class="funs-donmoi">
-	<div class="load-oldcustomer"><img src="upload/loads/load_1.gif" width="40px"></div>
-	<div class="add-order">
-		<span class="glyphicon glyphicon-plus-sign" aria-hidden="true"></span>
-	</div>
-</div>
